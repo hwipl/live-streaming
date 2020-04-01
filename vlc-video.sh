6b73f762a120728dc72af913c91804dc7e343857
hwipl <33433250+hwipl@users.noreply.github.com>
2020-04-01 19:14:19 +0200

    add vlc-video.sh
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ vlc-video.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+CMD=/usr/bin/cvlc
+INPUT=v4l2:///dev/video0
+FPS=5
+OUTPUT_HEIGHT=420
+STREAM_NAME="Desktop"
+STREAM_DEST_IP=224.10.8.1
+STREAM_DEST_PORT=5004
+OUTPUT=:sout=#transcode\{vcodec=h264,venc=x264\{preset=ultrafast,tune=zerolatency,intra-refresh,lookahead=10,keyint=15\},fps=$FPS,scale=auto,acodec=none,scodec=none,height=$OUTPUT_HEIGHT\}:rtp\{dst=$STREAM_DEST_IP,port=$STREAM_DEST_PORT,mux=ts,sdp=sap,name=$STREAM_NAME\}
+
+$CMD $INPUT $OUTPUT :sout-keep
