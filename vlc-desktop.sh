0288f77e6edd7650a6d39199ca987dfd70e1abae
hwipl <33433250+hwipl@users.noreply.github.com>
2020-04-01 18:53:46 +0200

    use variables in vlc-desktop.sh
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- vlc-desktop.sh
+++ vlc-desktop.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-cvlc screen:// \
-	':sout=#transcode{vcodec=h264,venc=x264{preset=ultrafast,tune=zerolatency,intra-refresh,lookahead=10,keyint=15},fps=5,scale=auto,acodec=none,scodec=none, height=720}:rtp{dst=224.10.8.1,port=5004,mux=ts,sdp=sap,name="Desktop"}' \
-	:sout-keep
+CMD=/usr/bin/cvlc
+INPUT=screen://
+STREAM_NAME="Desktop"
+STREAM_DEST_IP=224.10.8.1
+STREAM_DEST_PORT=5004
+OUTPUT=:sout=#transcode\{vcodec=h264,venc=x264\{preset=ultrafast,tune=zerolatency,intra-refresh,lookahead=10,keyint=15\},fps=5,scale=auto,acodec=none,scodec=none,height=720\}:rtp\{dst=$STREAM_DEST_IP,port=$STREAM_DEST_PORT,mux=ts,sdp=sap,name=$STREAM_NAME\}
+
+$CMD $INPUT $OUTPUT :sout-keep
